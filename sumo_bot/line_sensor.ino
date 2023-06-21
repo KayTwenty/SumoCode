a4faeb1a5360ba7c240a4bd666769e39704f05a9
Kay <davidanton037@gmail.com>
2023-06-21 13:56:42 -0400

    Better format on the line sensor

--- sumo_bot/line_sensor.ino
+++ sumo_bot/line_sensor.ino
@@ -1,22 +1,22 @@
-/* Note: 
-Higher values indicate a lighter surface (white line), 
-while lower values indicate a darker surface (the ring boundary).
-*/
+/**
+ * Handle the light sensor readings and correct the path if the robot moves outside the ring.
+ * Note: Higher values indicate a lighter surface (white line),
+ * while lower values indicate a darker surface (the ring boundary).
+ */
+void handleLightSensor() {
+  int lightSensorValue = analogRead(lightSensorPin);
 
-void handleLineSensor() {
-  int lineSensorValue = analogRead(lineSensorPin);
-
-  // Check if line is detected
-  if (lineSensorValue > lineThreshold) {
+  // Check if the robot is outside the ring
+  if (lightSensorValue < lightThreshold) {
     // Move backward to get back into the ring
     moveBackward();
     delay(correctionDuration);
 
     // Randomly choose left or right turn
     if (random(2) == 0) {
-      moveLeft(2);
+      moveLeft(2); // The argument could be a constant
     } else {
-      moveRight();
+      moveRight(2);
     }
 
     delay(correctionDuration);
