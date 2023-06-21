07455c52ad6727e358731aead11aef1d85bd2977
Kay <davidanton037@gmail.com>
2023-06-21 13:57:05 -0400

    More comments on motor controller

--- sumo_bot/motor_control.ino
+++ sumo_bot/motor_control.ino
@@ -1,4 +1,4 @@
-// Function to move the bot forward
+// Move the bot forward
 void moveForward() {
   digitalWrite(in1Pin, HIGH);
   digitalWrite(in2Pin, LOW);
@@ -6,7 +6,7 @@ void moveForward() {
   digitalWrite(in4Pin, LOW);
 }
 
-// Function to move the bot backward
+// Move the bot backward
 void moveBackward() {
   digitalWrite(in1Pin, LOW);
   digitalWrite(in2Pin, HIGH);
@@ -14,7 +14,7 @@ void moveBackward() {
   digitalWrite(in4Pin, HIGH);
 }
 
-// Function to turn the bot left
+// Turn the bot left by a specified degree
 void moveLeft(int degrees) {
   int turnDuration = (int)((float)degrees / 90.0 * 1000.0);
   
@@ -27,12 +27,17 @@ void moveLeft(int degrees) {
   stopMoving();
 }
 
-// Function to turn the bot right
-void moveRight() {
+// Turn the bot right by a specified degree.
+void moveRight(int degrees) {
+  int turnDuration = (int)((float)degrees / 90.0 * 1000.0);
+
   digitalWrite(in1Pin, HIGH);
   digitalWrite(in2Pin, LOW);
   digitalWrite(in3Pin, LOW);
   digitalWrite(in4Pin, HIGH);
+  delay(turnDuration);
+
+  stopMoving();
 }
 
 // Function to stop the bot
@@ -43,14 +48,16 @@ void stopMoving() {
   digitalWrite(in4Pin, LOW);
 }
 
-void forwardRotate() {
+// Hard Rotate
+void rotateClockwise() {
   digitalWrite(in1Pin, HIGH);
   digitalWrite(in2Pin, LOW);
   digitalWrite(in3Pin, LOW);
   digitalWrite(in4Pin, HIGH);
 }
 
-void reverseRotate() {
+// Hard Rotate
+void rotateCounterClockwise() {
   digitalWrite(in1Pin, LOW);
   digitalWrite(in2Pin, HIGH);
   digitalWrite(in3Pin, HIGH);
